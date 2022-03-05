5af9a069a1f17b0c0ae13b82525d56b3dc5c978f
Anshika Srivastava <67851072+Anshika-Srivastava@users.noreply.github.com>
2022-03-05 12:45:15 +0530

    Create Linked List Insertion.cpp

---
+++ Linked List/Linked List Insertion.cpp
@@ -0,0 +1,33 @@
+class Solution{
+  public:
+    //Function to insert a node at the beginning of the linked list.
+    Node *insertAtBegining(Node *head, int x) {
+       // Your code here
+
+       Node *new_node = new Node(x);
+
+       new_node->next = head;
+
+       head = new_node;
+
+       return head;
+    }
+    
+    
+    //Function to insert a node at the end of the linked list.
+    Node *insertAtEnd(Node *head, int x)  {
+       // Your code here
+       
+       Node *new_node = new Node(x);
+       if(head == NULL)
+       return new_node;
+       
+       Node *curr = head;
+       
+       while(curr->next != NULL){
+           curr = curr->next;
+       }
+       curr->next = new_node;
+       return head;
+    }
+};
