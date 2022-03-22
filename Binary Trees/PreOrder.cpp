3ea5add1c19060ef5f09b5688fa8fa41fccedf8e
Anshika Srivastava <67851072+Anshika-Srivastava@users.noreply.github.com>
2022-03-22 15:41:45 +0530

    Create PreOrder.cpp

---
+++ Binary Trees/PreOrder.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    void pre(TreeNode* root,vector<int>& ans){
+        if(root==NULL)
+            return;
+        ans.push_back(root->val);
+        pre(root->left,ans);
+        pre(root->right,ans);
+    }
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL)
+            return ans;
+        else
+            pre(root,ans);
+        return ans;
+    }
+};
